ae4ad62cf912a07235f9c3ab301d7206ca157740
Wang <hwang3028@panli-srv2.ece.gatech.edu>
2024-01-08 15:41:32 -0500

    .

--- result/LMC/paint_unlearning_sigma.py
+++ result/LMC/paint_unlearning_sigma.py
@@ -5,8 +5,8 @@ from adjustText import adjust_text
 
 sns.set_style("whitegrid")
 
-dataset = 'MNIST'
-# dataset = 'CIFAR10'
+# dataset = 'MNIST'
+dataset = 'CIFAR10'
 
 
 sigma_list = [0.05, 0.1, 0.2, 0.5, 1]
--- result/LMC/paint_utility_epsilon.py
+++ result/LMC/paint_utility_epsilon.py
@@ -6,24 +6,26 @@ from adjustText import adjust_text
 
 sns.set_style("whitegrid")
 
-#dataset = 'MNIST'
-dataset = 'CIFAR10'
+dataset = 'MNIST'
+#dataset = 'CIFAR10'
 
-K_dict = np.load('./'+str(dataset)+'/paint_utility_epsilon/K_list.npy', allow_pickle = True)
-if dataset == 'MNIST':
-    K_list = [17271, 2461, 1031, 421, 111]
-elif dataset == 'CIFAR10':
-    K_list = [20061, 2931, 1251, 521, 151]
+num_remove_list = [10, 300, 1000]
 epsilon_list = [0.1, 0.5, 1, 2, 5]
+K_dict = np.load('./'+str(dataset)+'/paint_utility_epsilon/K_list.npy', allow_pickle = True)
 
+for remove_idx, num_remove in enumerate(num_remove_list):
+    
+    K_list = []
+    for epsilon in epsilon_list:
+        K_list.append(K_dict[num_remove][epsilon])
 
-sns.lineplot(x = epsilon_list, y = K_list, marker='o', label = 'unlearn D\' from D')
-texts = []
-for i, (xi, yi) in enumerate(zip(epsilon_list, K_list)):
-    unlearn_acc_list = np.load('./'+str(dataset)+'/paint_utility_epsilon/acc_finetune_epsilon'+str(xi)+'.npy', allow_pickle = True)
-    texts.append(plt.text(xi, yi, f'accuracy={np.mean(unlearn_acc_list):.3f}', fontsize=10))
-adjust_text(texts)
-#plt.axhline(y=learn_acc, color='r', linestyle='--', label = 'learn D\' from scratch')
+    sns.lineplot(x = epsilon_list, y = K_list, marker='o', label = 'unlearn D\' from D, S='+str(num_remove))
+    texts = []
+    for i, (xi, yi) in enumerate(zip(epsilon_list, K_list)):
+        unlearn_acc_list = np.load('./'+str(dataset)+'/paint_utility_epsilon/'+str(num_remove)+'/acc_finetune_epsilon'+str(xi)+'.npy', allow_pickle = True)
+        texts.append(plt.text(xi, yi, f'accuracy={np.mean(unlearn_acc_list):.3f}', fontsize=10))
+    adjust_text(texts)
+    #plt.axhline(y=learn_acc, color='r', linestyle='--', label = 'learn D\' from scratch')
 plt.legend(fontsize=15)
 plt.xlabel(r'$\epsilon$', fontsize=15)
 plt.ylabel('K', fontsize=15)
